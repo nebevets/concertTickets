994a3cf7dd346b9d47c6099f4d8303b74371a90a
nebevets <steveben@exmsft.com>
2019-03-05 11:14:55 -0800

    changed App to show initial content

--- src/App.js
+++ src/App.js
@@ -1,25 +1,18 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import mbm from './images/1992.11.29_meatBeatManifesto_orbital.png';
 import './App.css';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+        <h1>welcome to concert tickets</h1>
+        <div className="ticket">
+          <img src={mbm} alt="ticket" />
+          <div className="description">
+            meat beat manifesto w/ orbital at masquerade club, atlanta, 1992.
+          </div>
+        </div>
       </div>
     );
   }
